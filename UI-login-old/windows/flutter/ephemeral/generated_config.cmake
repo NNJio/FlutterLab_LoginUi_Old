7d017c626895f2d5228cf2ebfc68366441f77d0d
Phatsakorn Chosungnoen <99648810+NNJio@users.noreply.github.com>
2022-08-17 02:43:07 +0700

    Add files via upload

---
+++ UI-login-old/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login"
+  "FLUTTER_TARGET=C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\NNJio\\Desktop\\Flutter Lab\\LoginSystem\\login\\.dart_tool\\package_config.json"
+)
